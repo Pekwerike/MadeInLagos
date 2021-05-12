31653e6c3ce6191ceacbf483d283d2397cd048e6
Prosper Ekwerike <pekwerike@gmail.com>
2021-05-12 15:05:48 +0100

    Code refactor

--- app/src/main/java/com/pekwerike/madeinlagos/mappers/ProductMappers.kt
+++ app/src/main/java/com/pekwerike/madeinlagos/mappers/ProductMappers.kt
@@ -8,47 +8,42 @@ This file contains extension functions that makes it easier to convert between P
 and Product types
 */
 
-fun List<ProductEntity>.toProductList(): List<Product> {
-    return map { productEntity: ProductEntity ->
-        Product(
-            id = productEntity.productId,
-            name = productEntity.name,
-            description = productEntity.description,
-            currency = productEntity.currency,
-            price = productEntity.price
-        )
-    }
-}
-
-fun List<Product>.toProductEntityList(): List<ProductEntity> {
-    return map { product: Product ->
-        ProductEntity(
-            productId = product.id,
-            name = product.name,
-            description = product.description,
-            currency = product.currency,
-            price = product.price
-        )
-    }
-}
-
-fun ProductEntity.toProduct(): Product {
-    return Product(
-        id = productId,
-        name = name,
-        description = description,
-        currency = currency,
-        price = price
+fun List<ProductEntity>.toProductList(): List<Product> = map { productEntity: ProductEntity ->
+    Product(
+        id = productEntity.productId,
+        name = productEntity.name,
+        description = productEntity.description,
+        currency = productEntity.currency,
+        price = productEntity.price
     )
 }
 
-fun Product.toProductEntity(): ProductEntity {
-    return ProductEntity(
-        productId = id,
-        name = name,
-        description = description,
-        currency = currency,
-        price = price
+
+fun List<Product>.toProductEntityList(): List<ProductEntity> = map { product: Product ->
+    ProductEntity(
+        productId = product.id,
+        name = product.name,
+        description = product.description,
+        currency = product.currency,
+        price = product.price
     )
 }
+
+
+fun ProductEntity.toProduct(): Product = Product(
+    id = productId,
+    name = name,
+    description = description,
+    currency = currency,
+    price = price
+)
+
+
+fun Product.toProductEntity(): ProductEntity = ProductEntity(
+    productId = id,
+    name = name,
+    description = description,
+    currency = currency,
+    price = price
+)
 
